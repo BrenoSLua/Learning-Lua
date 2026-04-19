6c12bfb72eef02add191210fffe62fee9e21a8e3
Breno Lima <brenosobral500@gmail.com>
2026-04-19 15:41:01 -0300

    5 exercicios feito

---
+++ raphi.lua
@@ -0,0 +1,68 @@
+-- Exercicios chat gpt
+-- Esses exercicios estão salvos no meu discord (o link não está disponível)
+
+--===============================================================--
+
+--===============================================================--
+ -- 1 EXERCICOS
+local name = "Breno"
+local age = 16
+
+print ("Oi, ".. name ..". Sua idade e: ".. age)
+
+
+
+--===============================================================--
+
+-- 2 EXERCICOS
+local number1 = 2
+local number2 = 2
+
+print("A soma e: ".. number1 + number2)
+
+
+--===============================================================--
+
+-- 3 EXERCICOS
+--print("Digite seu nome")
+
+--local nome = io.read("*line")
+
+--print("Bem vindo: ".. nome)
+
+
+--===============================================================--
+
+
+-- 4 EXERCICOS
+
+--print("Digite um numero: ")
+
+--local number3 = io.read("*number")
+
+--if number3 > 10 then
+--    print("Seu numero e maior que 10")
+--elseif number3 <= 10 then
+--    print("Seu numero e igual a 10")
+--else
+--    print("Seu numero e igual 10")
+--end
+
+
+--===============================================================--
+
+
+-- 5 EXERCICOS
+--print("Digite um numero: ")
+
+--local parImpar = io.read("*number")
+
+--if (parImpar % 2 == 0) then
+--   print("Seu numero e Par")
+--else
+--    print("Seu numero e Impar")
+--end
+
+
+--===============================================================--
+
