925d7cfcf263cf27bff7e08b4d08c6bd67073eeb
Breno Lima <brenosobral500@gmail.com>
2026-04-24 10:05:42 -0300

    Operadores aritméticos(+ -)

--- Udemy/Aula04.lua
+++ Udemy/Aula04.lua
@@ -1,3 +1,53 @@
 --  Operadores artméticos:
 -- ex: -, +, /, *, %, ^
 
+-- +
+
+print(9 + 9)
+
+-- "-"
+
+print(8 - 7)
+
+print(-4)-- aqui diz que este número é negativo
+
+-- * 
+
+print(45 * 4)
+
+-- /
+
+print(10 / 20)
+print(10 / 5)
+
+-- % (mostra o resto da divisão)
+
+print(10 % 5) -- ou seja 0
+
+-- ^ (exponeiciação)
+
+print(2^2) --  ou seja 4
+
+
+-------------------------------------------------------------------
+
+-- também posso colcoar em variáveis:
+local soma = 5 + 5
+print(soma)
+
+-- ou 
+
+local n1 = 3
+local n2 = 3
+
+local soma1 = n1 + n2
+
+print(soma1)
+
+-- aqui também cabe com regras da matamática como:
+
+print(5 + 5 * 5)-- 5 vezes 5 mais 5
+
+-- se fosse entre parenteses:
+
+print((5 + 5) * 10) -- primeiro parenteses depois o 10
