0816780ad8a611563f90a4179ee7da9173b2719e
Breno Lima <brenosobral500@gmail.com>
2026-05-07 19:16:21 -0300

    exercicio5 feito

--- Udemy/Aula09.lua
+++ Udemy/Aula09.lua
@@ -34,3 +34,9 @@ local txt3 = "Ola, mundo"
 local sub = string.sub(txt3, 6,10) -- vou colocar que pelavra começa no lugar do 6 e onde termina que é o 10
 
 print(sub)
+
+--- string.gsub() -> substitui uma palavra por outra
+--- string.gsub(texto, "palavra a ser substituida", "palavra nova") 
+local txt5 = "Ola, mundo"
+local sub2 = string.gsub(txt5, "lua", "mundo")
+print(sub2)
--- exercicio/ex5.lua
+++ exercicio/ex5.lua
@@ -1,20 +1,21 @@
 -- Faça um programa que peça a altura e o comprimento de um quadrilátero e mostre a sua área.  
 
-print("Coloque virgula pra números decimais")
+print("Calcule a area do seu quadrado/retangulo")
 
 print("Qual a altura do seu quadrado/retangulo?")
-local altura = (io.read("*n"))
+local altura = (io.read())
 
 print("Qual o comprimento do seu quadrado/retangulo?")
-local com = (io.read("*n"))
+local com = (io.read())
 
-local vi = string.gsub(altura, ",",".")
-local vi2 = string.gsub(com, ",",".")
+local altura = string.gsub(altura, ",",".")
+local com = string.gsub(com, ",",".")
+
+local altura = tonumber(altura)
+local com = tonumber(com)
 
 
-
-
-local area = vi * vi2
+local area = altura * com 
 
 print("A area do seu quadrado/retangulo e: ".. area)
 
