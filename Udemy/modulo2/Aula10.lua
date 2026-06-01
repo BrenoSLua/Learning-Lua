c9008ef9d64eed8d334ae46c3d45925c05798201
Breno Lima <brenosobral500@gmail.com>
2026-05-31 21:24:20 -0300

    Operadores Relacionais, modolo 2

---
+++ Udemy/modulo2/Aula10.lua
@@ -0,0 +1,42 @@
+--------------------- OPERADORES RELACIONAIS 
+
+-- ==  / ~= 'Diferente' / < menor / > maior / <= menor ou igual / >= maior ou igual
+--[[ equivalencia
+local var1 = 5 == 5
+local var2 = 5 == 6
+
+print(var1, var2)
+
+]]
+
+
+
+
+
+--[[  diferenncia
+
+local var3 = 5 ~= 5
+local var4 = 5 ~= 6 
+
+print(var3, var4)
+
+]]
+
+
+
+
+--[[menor / maior
+
+local var5 = 5 < 5
+local var6 = 5 > 6
+print (var5, var6)
+
+]]
+
+
+--[[ menor iguual / maior igual]]
+
+local var7 = 5 <= 5
+local var8 = 5 >= 7
+
+print (var7, var8)
